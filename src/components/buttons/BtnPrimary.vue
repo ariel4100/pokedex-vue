7f5978b1c56778c72f5cf287bd0919e79de22716
ariel4100 <arielcallisaya00@gmail.com>
2023-12-27 08:53:09 -0300

    first commit

--- src/components/buttons/BtnPrimary.vue
+++ src/components/buttons/BtnPrimary.vue
@@ -34,7 +34,7 @@
       </span>
     </button>
     <RouterLink v-else v-slot="{  isActive, isExactActive }" :to="href" class="inline-flex text-lg">
-      <span v-if="active" :class="isActive  ? 'bg-[#F22539]' : 'bg-[#BFBFBF]', width ? 'md:min-w-[275px]' : ''"  class="bg-[#F22539] text-white font-bold  rounded-full  min-w-[131px] h-[44px] px-4 inline-flex justify-center items-center">
+      <span v-if="active" :class="isActive  ? 'bg-[#F22539]' : 'bg-[#BFBFBF]', width ? 'md:min-w-[275px]' : ''"  class="  text-white font-bold  rounded-full  min-w-[131px] h-[44px] px-4 inline-flex justify-center items-center">
         <slot name="icon"></slot> 
         {{title}} 
       </span>
